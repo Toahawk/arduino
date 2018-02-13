1f39470be6d5014f374a135d7b2e80e5f548ab97
Attila Toth <attila_toth1@epam.com>
2018-02-13 14:45:21 +0100

    Second version

---
+++ arduino2.cpp
@@ -0,0 +1,81 @@
+#include "Arduino.h"
+
+#define emergencyStopPin 2
+#define moveUpPin 12
+#define moveDownPin 13
+
+#define directionPin 4
+#define phasePin 7
+
+volatile boolean stopped = false;
+
+void setup()
+{
+	pinMode(emergencyStopPin, INPUT);
+	attachInterrupt(digitalPinToInterrupt(emergencyStopPin), emergencyStopChange, CHANGE);
+
+	pinMode(moveUpPin, INPUT);
+	pinMode(directionPin, OUTPUT);
+	pinMode(phasePin, OUTPUT);
+	boolean movingUp = false;
+	boolean movingDown = false;
+	boolean wasAlreadyStop = false;
+}
+
+
+
+
+void loop()
+{
+	boolean moveUp = digitalRead(moveUpPin);
+	boolean moveDown = digitalRead(moveDownPin);
+
+	if (!stopped) {
+		if (movingUp || moveUp) {
+			movingUp = true;
+			movingDown = false;
+			digitalWrite (directionPin, HIGH);
+			digitalWrite (phasePin, HIGH);
+		}
+		if (movingDown || moveDown) {
+			movingUp = false;
+			movingDown = true;
+			digitalWrite (directionPin, LOW);
+			digitalWrite (phasePin, HIGH);
+		}
+	} else {
+
+		digitalWrite (directionPin, LOW);
+		digitalWrite (phasePin, LOW);	
+
+		if (movingUp && moveDownPin) {
+			stopped = !stopped;
+			movingUp = false;
+			movingDown = true;
+			digitalWrite (directionPin, LOW);
+			digitalWrite (phasePin, HIGH);
+		}
+		if (movingDown && moveUpPin) {
+			stopped = !stopped;
+			movingUp = true;
+			movingDown = false;
+			digitalWrite (directionPin, HIGH);
+			digitalWrite (phasePin, HIGH);
+		}		
+	}
+	
+	if (wasAlreadyStop && digitalRead(moveUpPin)) {
+		digitalWrite (directionPin, HIGH);
+		digitalWrite (phasePin, HIGH);
+		delay (5);
+		digitalWrite (directionPin, LOW);
+		digitalWrite (phasePin, LOW);	
+		wasAlreadyStop = false;
+	}	
+	delay(10);
+}
+
+void emergencyStopChange() {
+	stopped = true;
+	wasAlreadyStop = true;
+}
